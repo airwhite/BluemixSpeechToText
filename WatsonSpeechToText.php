78f8d4cc4e68b1d0ce5f010d038ebad2d36f1731
airwhite <airwhite@airwhite.net>
2017-10-15 16:55:29 +0900

    Update WatsonSpeechToText.php

--- WatsonSpeechToText.php
+++ WatsonSpeechToText.php
@@ -1,10 +1,8 @@
 <?php
 //
-//  IBM Watson Speech to Text
+//  IBM Watson API Speech to Text
 //
 
-define("WTS_USERNAME",    "<Watson Username>");
-define("WTS_PASSWORD",    "<Watson Password>");
 define("WTS_BASE_URL",    "https://stream.watsonplatform.net");
 define("WTS_SERVICE",     "/speech-to-text");
 define("WTS_VERSION",     "/api/v1/recognize");
@@ -12,7 +10,7 @@ define("WTS_NARROW_BAND", "ja-JP_NarrowbandModel");
 define("WTS_BROAD_BAND",  "ja-JP_BroadbandModel");
 define("WTS_REQUEST_URL", WTS_BASE_URL.WTS_SERVICE.WTS_VERSION."?model=".WTS_NARROW_BAND);
 
-function Speech_to_Text($audioFile, $audioType) {
+function Speech_to_Text($user, $pass, $audioFile, $audioType) {
     $size = filesize($audioFile);
     $data = file_get_contents($audioFile);
     $ch = curl_init();
@@ -22,7 +20,7 @@ function Speech_to_Text($audioFile, $audioType) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, WTS_REQUEST_URL);
-    curl_setopt($ch, CURLOPT_USERPWD, WTS_USERNAME.":".WTS_PASSWORD);
+    curl_setopt($ch, CURLOPT_USERPWD, "$user:$pass");
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         "Content-Type: $audioType",
         "Transfer-Encoding: chunked"
